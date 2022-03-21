8e67925c7f578aab763eb6ef938cdfd14cbb1a2f
Raj Patel <36126350+rajp6265@users.noreply.github.com>
2022-03-21 18:31:06 -0400

    integrated code

---
+++ Assets/Scripts/UI/Core/UIBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBase : MonoBehaviour
+{
+    private Canvas _canvas;
+    private bool _isActive;
+    public bool IsActive => _isActive;
+
+    void Awake()
+    {
+        _canvas = GetComponent<Canvas>();
+
+        // Show();
+    }
+
+    public virtual void Enable()
+    {
+        _isActive = _canvas.enabled = true;
+    }
+
+    public virtual void Disable()
+    {
+        _isActive = _canvas.enabled = false;
+    }
+
+    public void Show()
+    {
+        if (_isActive) return;
+        Enable();
+    }
+    public void Hide()
+    {
+
+        Disable();
+    }
+    private void OnDestroy()
+    {
+        _canvas = null;
+    }
+}
---
+++ Assets/Scripts/Util/ShowToast.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+public class ShowToast
+{
+    public async static void ShowFadingToast(GameObject ui, float time = 0.5f)
+    {
+        CanvasGroup cg = null;
+        if ((cg = ui.GetComponent<CanvasGroup>()) == null)
+        {
+            cg = ui.AddComponent<CanvasGroup>();
+        }
+        cg.alpha = 1;
+        while (cg.alpha != 0)
+        {
+            await Task.Delay((int)(1000 * time));
+            cg.alpha -= 0.1f;
+        }
+    }
+
+}
