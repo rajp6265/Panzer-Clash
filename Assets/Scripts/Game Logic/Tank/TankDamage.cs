d7e528ccd36b07071684a12465b512d761d42fc1
Sanatkumar Dhobi <sanat541997@gmail.com>
2022-03-21 19:22:26 -0400

    Tank
    Tank movement
    Tank Damage
    Tank Cannon Movement
    Tank Data

---
+++ Assets/Scripts/Game Logic/Tank/TankDamage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TankDamage : MonoBehaviour
+{
+    [SerializeField]
+    private float _health = 100;
+    private TankData _tankData;
+
+    private void Awake()
+    {
+        _tankData = GetComponentInParent<TankData>();
+    }
+    public void TryApplyDamage(float damageAmount)
+    {
+        _health -= damageAmount;
+        if (_health <= 0)
+        {
+            GameEvents.OnTankDestroyed?.Invoke(_tankData);
+        }
+    }
+
+
+}
