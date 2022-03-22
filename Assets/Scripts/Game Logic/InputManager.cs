0dd9713fa7de02689bb68d94694bcedcfc549377
jahnavi24shah <40710710+jahnavi24shah@users.noreply.github.com>
2022-03-22 00:11:12 -0400

    Add files via upload

---
+++ Assets/Scripts/Game Logic/InputManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    private Vector2 tankMovementInput = Vector2.zero;
+    private Vector2 tankCannonMovementInput = Vector2.zero;
+
+    [SerializeField]
+    private VariableJoystick tankMovementJoystick;
+    [SerializeField]
+    private VariableJoystick tankCannonMovementJoystick;
+
+    private void Update()
+    {
+        GetInputs();
+    }
+
+    private void GetInputs()
+    {
+
+        if (tankMovementJoystick.Horizontal != tankMovementInput.x || tankMovementJoystick.Vertical != tankMovementInput.y)
+        {
+            tankMovementInput.x = tankMovementJoystick.Horizontal * Time.deltaTime;
+            tankMovementInput.y = tankMovementJoystick.Vertical * Time.deltaTime;
+            GameEvents.OnTankMovementChanged?.Invoke(tankMovementInput);
+        }
+        if (tankCannonMovementJoystick.Horizontal != tankCannonMovementInput.x || tankCannonMovementJoystick.Vertical != tankCannonMovementInput.y)
+        {
+            tankCannonMovementInput.x = tankCannonMovementJoystick.Horizontal * Time.deltaTime;
+            tankCannonMovementInput.y = tankCannonMovementJoystick.Vertical * Time.deltaTime;
+            GameEvents.OnTankCannonMovementChanged?.Invoke(tankCannonMovementInput);
+        }
+    }
+}
