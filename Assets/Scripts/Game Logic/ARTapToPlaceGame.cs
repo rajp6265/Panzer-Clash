19d96a67ee95220adf61445f0f1311b42c15d196
Aakash Pandya <54207547+aakashpandya198@users.noreply.github.com>
2022-03-26 00:00:36 -0400

    Add files via upload

---
+++ Assets/Scripts/Game Logic/ARTapToPlaceGame.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARTapToPlaceGame : MonoBehaviour
+{
+    [SerializeField] private GameSceneData _gameSceneDataPrefab;
+    [SerializeField] private GameObject _crosshair;
+
+    private GameSceneData _spawnedGameSceneData = null;
+    private ARRaycastManager _aRRaycastManager;
+    private Vector2 _touchPosition;
+    private bool planeFound = false;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+
+    private void Awake()
+    {
+        _aRRaycastManager = GetComponent<ARRaycastManager>();
+
+    }
+
+    bool GetTouchPositionData(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+
+    private void Update()
+    {
+        if (!GetTouchPositionData(out _touchPosition) && planeFound)
+            return;
+
+        if (_aRRaycastManager.Raycast(_touchPosition, hits, TrackableType.PlaneWithinPolygon) && _crosshair.activeInHierarchy)
+        {
+            _crosshair.SetActive(false);
+            var hitPose = hits[0].pose;
+            if (_spawnedGameSceneData == null)
+            {
+                _spawnedGameSceneData = Instantiate(_gameSceneDataPrefab, hitPose.position, hitPose.rotation);
+                planeFound = true;
+            }
+            else
+            {
+                _spawnedGameSceneData.gameObject.transform.position = hitPose.position;
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Game Logic/GameSceneData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GameSceneData : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GameEvents.OnGameStarted?.Invoke();
+    }
+}
