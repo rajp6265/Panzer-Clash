dda03b1feef53104257ba9919d4804c30ad09e62
rahilshah07 <70007206+rahilshah07@users.noreply.github.com>
2022-04-04 15:20:32 -0400

    Add files via upload

---
+++ Assets/Scripts/UI/Screens/MainMenuScreen.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScreen : UIScreen
+{
+    [SerializeField] private Button _playButton;
+    [SerializeField] private Button _exitButton;
+
+    private void Start()
+    {
+        _playButton.onClick.AddListener(StartGame);
+        _exitButton.onClick.AddListener(EndGame);
+    }
+
+    private void StartGame()
+    {
+        UIManager.ChangeScreen(ScreenType.None);
+    }
+    private void EndGame()
+    {
+        Application.Quit();
+    }
+    private void OnDestroy()
+    {
+        _playButton.onClick.RemoveListener(StartGame);
+        _exitButton.onClick.RemoveListener(EndGame);
+    }
+}
